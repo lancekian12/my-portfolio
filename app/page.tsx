ded56aac2d20def61e863a07fab76fc81f3a9624
Lance Kian Flores <lancekian12@gmail.com>
2026-05-11 00:27:56 +0800

    fix: comment out for now

--- app/page.tsx
+++ app/page.tsx
@@ -98,9 +98,9 @@ export default function Home() {
         >
         </motion.div> */}
       </div>
-      <div className="fixed bottom-4 right-4 z-50">
+      {/* <div className="fixed bottom-4 right-4 z-50">
         <ChatBox />
-      </div>
+      </div> */}
     </main>
   );
 }
